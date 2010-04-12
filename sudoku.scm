933aef9da7c8937b1a7147074acb124a70aefe65
Christopher League <league@contrapunctus.net>
2010-04-12 17:24:30 -0400

    Added keyboard focus to Sudoku widget
    SVN r7 2008-04-14 20:28:11 -0400

--- sudoku.scm
+++ sudoku.scm
@@ -10,7 +10,7 @@
 (define sudoku%
   (class ac3algo%
     (override constraint-on constraint-between partners-of)
-    (init-field (n 2))
+    (init-field (n 2) (nn (* n n)))
     (define (enum i)
       (if (= i 0) null
           (cons i (enum (- i 1)))))
@@ -47,7 +47,7 @@
 (define sudoku-cell-view%
   (class canvas%
     (public update)
-    (override on-event)
+    (override on-char on-focus)
     (init-field 
      (puz (new sudoku%))
      (id '(1 . 1)))
@@ -58,6 +58,10 @@
           (set! cache d)
           (send this refresh))))
     (define (paint-me cv dc)
+      (when (send this has-focus?)
+        (send dc set-brush "yellow" 'solid)
+        (send dc draw-rectangle 0 0
+              (send this get-width) (send this get-height)))
       (cond
         ((null? cache)        ; no solution
          (send dc set-brush "red" 'solid)
@@ -79,19 +83,29 @@
               ('8 (send dc draw-rectangle 10 16 2 2))
               ('9 (send dc draw-rectangle 16 16 2 2))))
           cache))))
-    (define n2 (square (get-field n puz)))
-    (define (on-event ev)
-      (case (send ev get-event-type)
-        ('left-down 
-          (let ((d (send puz domain-of id))
-                (vs (get-field values puz)))
-            (send puz set-domain-of id
-                  (cond
-                    ((null? d) vs)
-                    ((null? (cdr d))
-                     (if (= (car d) n2) vs
-                         (list (+ (car d) 1))))
-                    (else (list (car d)))))))))
+    (define (on-char ch)
+      (case (send ch get-key-code)
+        ((#\1 numpad1) (set-domain '(1)))
+        ((#\2 numpad2) (set-domain '(2)))
+        ((#\3 numpad3) (set-domain '(3)))
+        ((#\4 numpad4) (set-domain '(4)))
+        ((#\5 numpad5) (set-domain '(5)))
+        ((#\6 numpad6) (set-domain '(6)))
+        ((#\7 numpad7) (set-domain '(7)))
+        ((#\8 numpad8) (set-domain '(8)))
+        ((#\9 numpad9) (set-domain '(9)))
+        ((#\space) (set-domain (get-field values puz)))
+        ((up) (refocus 'up))
+        ((down) (refocus 'down))
+        ((left) (refocus 'left))
+        ((right) (refocus 'right))))
+    (define (set-domain d)
+      (send puz set-domain-of id d)
+      (refocus 'next))
+    (define (refocus dir)
+      (send (send (send this get-parent) get-parent) refocus id dir))
+    (define (on-focus on?)
+      (send this refresh))
     (super-new
      (min-width 32) (min-height 32)
      (paint-callback paint-me) (style '(border)))
@@ -99,27 +113,44 @@
      
 (define sudoku-row-view%
   (class horizontal-pane%
-    (init-field (puz (new sudoku%)) (row 1))
+    (init-field (puz (new sudoku%)) (row 1) (cells #f))
+    (define (each-cell i)
+      (let* ((j (+ i 1))
+             (c (new sudoku-cell-view% (puz puz) (id (cons row j))
+                     (parent this))))
+        (when (zero? (modulo j (get-field n puz)))
+          (new canvas% (parent this) (min-width 2) (min-height 2)))
+        c))
     (super-new)
     (new message% (label (number->string row)) (parent this))
-    (for-loop 
-     1 (+ (square (get-field n puz)) 1)
-     (lambda (c) 
-       (new sudoku-cell-view% (puz puz) (id (cons row c))
-            (parent this))
-       (when (zero? (modulo c (get-field n puz)))
-         (new canvas% (parent this) (min-width 2) (min-height 2)))))))
+    (set! cells (vector-tabulate (get-field nn puz) each-cell))))
 
 (define sudoku-grid-view%
   (class vertical-pane%
-    (init-field (puz (new sudoku%)))
+    (public refocus)
+    (init-field (puz (new sudoku%)) (rows #f))
+    (define (refocus id dir)
+      (let ((r (- (car id) 1))
+            (c (- (cdr id) 1)))
+        (case dir
+          ((right next) (set! c (+ c 1)))
+          ((left) (set! c (- c 1)))
+          ((up) (set! r (- r 1)))
+          ((down) (set! r (+ r 1))))
+        (when (and (eq? dir 'next) (>= c (get-field nn puz)))
+          (set! c 0)
+          (set! r (+ r 1)))
+        (set! r (modulo r (get-field nn puz)))
+        (set! c (modulo c (get-field nn puz)))
+        (send (vector-ref (get-field cells (vector-ref rows r)) c) focus)))
+    (define (each-row i)
+      (let* ((j (+ i 1))
+             (r (new sudoku-row-view% (puz puz) (row j) (parent this))))
+       (when (zero? (modulo j (get-field n puz)))
+         (new canvas% (parent this) (min-width 2) (min-height 2)))
+        r))
     (super-new)
-    (for-loop
-     1 (+ (square (get-field n puz)) 1)
-     (lambda (r) 
-       (new sudoku-row-view% (puz puz) (row r) (parent this))
-       (when (zero? (modulo r (get-field n puz)))
-         (new canvas% (parent this) (min-width 2) (min-height 2)))))))
+    (set! rows (vector-tabulate (get-field nn puz) each-row))))
 
 (define sudoku-frame%
   (class frame%
